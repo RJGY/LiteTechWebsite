70f0ea38ba8e5f3342703a78752a123e186c64bc
RJGY <rjgy2yu@gmail.com>
2020-11-21 16:44:59 +1100

    readded import statement

--- src/pages/account.js
+++ src/pages/account.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
+import DiscordOauth2 from 'discord-oauth2'
 
 const SecondPage = () => (
   <Layout>
